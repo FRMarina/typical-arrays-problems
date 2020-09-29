f28be2c0077dc14b2eb33fe0e21d6f1affaa09ac
FRMarina <marinafedchik@gmail.com>
2020-09-29 18:06:33 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
+exports.min = function min(array) {
+    if (arguments.length === 0) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.min.apply(null, array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (arguments.length === 0) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.max.apply(null, array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (arguments.length === 0) {
+        return 0;
+    } else if (array.length === 0) {
+        return 0;
+    } else {
+        return (
+            array.reduce(function (sum, curr) {
+                return sum + curr;
+            }) / array.length
+        );
+    }
+};
